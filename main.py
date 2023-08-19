0a7edd609e7c227429c5f9c352bb194b46fdecef
nicolaromero <nicolaromerogerik@gmail.com>
2023-08-19 12:29:28 -0300

    Optimización del código. Se agrega una función para ser reutilizada en cualquier momento y se pasa a una sola variable nav_risk donde se permite de ahora en adelante consultar el profit en rangos de fechas más amplios.

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ funds = [
     }
 ]
 
-# Ahora, creo un bucle "for" en el que se itera en cada posición del array y realiza una llamada a la API de Fintual obtener la info para cada fondo:
-for fund in funds:
+
+def get_profit(fund, from_date, to_date):
 
     fund_name = fund['name']
     fund_id = fund['id']
@@ -35,16 +35,19 @@ for fund in funds:
     response = requests.get(url)
     responseData = response.json()
 
-    # La llamada puede resultar en un objeto muy extenso, pero en este caso para calcular la rentabilidad, necesitamos solo el atributo "nav" o "net asset value" que representa el valor cuota de cada uno de los fondos:
-    nav_riksy_11 = float(responseData['data']
-                         [0]['attributes']['net_asset_value'])
-    nav_riksy_12 = float(responseData['data']
-                         [1]['attributes']['net_asset_value'])
+    # La llamada puede resultar en un objeto muy extenso, pero en este caso para calcular la rentabilidad, necesitamos solo el atributo "nav" o "net asset value" que representa el valor cuota de cada uno de los fondos. Con un ciclo "for" recorremos todas las dias consultados:
+    nav_risky = [float(data['attributes']['net_asset_value'])
+                 for data in responseData['data']]
 
-    # PD: Se usa el metodo float para pasar la respuesta de formato string a numerico, para poder asi calcular el rendimiento la formula entregada:
+    # PD: Se usa el metodo float para pasar la respuesta de formato string a numerico, para poder asi calcular el rendimiento la formula entregada. Se calcula el profit entre la primera y ultima posición obtenida en el array "nav_risk". De esta manera se puede calcular el profit en rangos de fechas mas amplios si se lo deseara:
 
-    profit = (nav_riksy_12 - nav_riksy_11)/nav_riksy_11
+    profit = (nav_risky[-1] - nav_risky[0])/nav_risky[0]
 
     # Finalmente, se imprime el resultado por consola para cada uno de los fondos:
     print(
         f'La rentabilidad 📈 de nuestro fondo {fund_name} el {to_date} fue de: {round(profit, 4)}%')
+
+
+# Ahora, creo un bucle "for" en el que se itera en cada posición del array y realiza una llamada a la API de Fintual obtener la info para cada fondo:
+for fund in funds:
+    get_profit(fund, from_date, to_date)
